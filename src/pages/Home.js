dac252f4c1c2467e7f07f51dcb769866cdd82315
Jonathan Nazareth <jonathannazareth@hotmail.co.uk>
2023-03-10 14:29:39 +0000

    Fetching data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 
 // components
-// import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutDetails from "../components/WorkoutDetails"
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null)
@@ -22,8 +22,8 @@ const Home = () => {
   return (
     <div className="home">
       <div className="workouts">
-        {workouts && workouts.map((workout) => (
-          <p key={workout._id}>{workout.title}</p>
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
         ))}
       </div>
     </div>
