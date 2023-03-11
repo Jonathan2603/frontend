1d045f3add384331a9f0a17bb46b6df716087e71
Jonathan Nazareth <jonathannazareth@hotmail.co.uk>
2023-03-11 00:42:05 +0000

    Deleting Data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react"
-import { useWorkoutsContext} from "../hooks/useWorkoutsContext"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-  const {workouts, dispatch} = useWorkoutsContext()
+  const { workouts, dispatch } = useWorkoutsContext()
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -19,7 +19,7 @@ const Home = () => {
     }
 
     fetchWorkouts()
-  }, [])
+  }, [dispatch])
 
   return (
     <div className="home">
